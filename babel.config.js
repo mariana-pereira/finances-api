c0d3ccb4593eea475fed2b172484437bb2033516
Mariana <marianadealmeidapereira@outlook.com>
2021-01-16 14:41:36 -0300

    add babel configuration

---
+++ babel.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current',
+        }
+      }
+    ],
+    '@babel/preset-typescript'
+  ],
+  plugins: [
+    "babel-plugin-transform-typescript-metadata",
+    ['module-resolver', {
+      alias: {
+        '@config': './src/config',
+        '@controllers': './src/controllers',
+        '@models': './src/models',
+        '@views': './src/views'
+      }
+    },],
+    ["@babel/plugin-proposal-decorators", { "legacy": true }],
+    ['@babel/plugin-proposal-class-properties', { loose: true }],
+  ],
+  ignore: [
+    '**/*.spec.ts'
+  ]
+}
