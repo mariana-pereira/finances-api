2731acdeac7a86c0ef07af9ba13cfb81ed31d593
Mariana <marianadealmeidapereira@outlook.com>
2021-05-15 12:38:50 -0300

    create user migration

---
+++ src/database/migrations/1621091932945-CreateUser.ts
@@ -0,0 +1,59 @@
+
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateUsers1614120762804 implements MigrationInterface {
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
+    await queryRunner.createTable(
+        new Table({
+          name: 'users',
+          columns: [
+            {
+              name: 'id',
+              type: 'uuid',
+              isPrimary: true,
+              generationStrategy: 'uuid',
+              default: 'uuid_generate_v4()',
+            },
+            {
+              name: 'name',
+              type: 'varchar'
+            },
+            {
+              name: 'email',
+              type: 'varchar'
+            },
+            {
+              name: 'password_hash',
+              type: 'varchar',
+            },
+            {
+              name: 'token',
+              type: 'varchar',
+            },
+            {
+              name: 'token_created_at',
+              type: 'timestamp',
+              default: 'now()'
+            },
+            {
+              name: 'created_at',
+              type: 'timestamp',
+              default: 'now()'
+            },
+            {
+              name: 'updated_at',
+              type: 'timestamp',
+              default: 'now()'
+            }
+          ]
+        })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('users');
+  }
+
+}
