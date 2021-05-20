9a02deb63b5dd608f9f4f0c8290dde0f2bf83c9f
Mariana <marianadealmeidapereira@outlook.com>
2021-05-20 08:46:49 -0300

    add show user method to user controller

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,19 +1,36 @@
 import { Request, Response } from 'express';
 
-import UserService from '../services/createUserService';
+import CreateUserService from '../services/createUserService';
+import ShowUserService from '../services/showUserService';
 
 class UserController {
   public async store (request: Request, response: Response): Promise<Response> {
     try {
       const { name, email, password_hash } = request.body;
 
-      const createUser = new UserService();
+      const createUser = new CreateUserService();
 
       const user = await createUser.execute({
         name,
         email,
         password_hash,
       });
+
+      delete user.password_hash;
+
+      return response.status(200).json(user);
+    } catch (error) {
+      return response.status(400).json({ error: error.message });
+    }
+  }
+
+  public async show (request: Request, response: Response): Promise<Response> {
+    try {
+      const { id } = request.params;
+
+      const showUser = new ShowUserService();
+
+      const user = await showUser.execute({ id });
 
       delete user.password_hash;
 
