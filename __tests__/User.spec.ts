2f15a8e9f9f84243d59fcacc5ed3d4f0bcf2385a
Mariana <marianadealmeidapereira@outlook.com>
2021-05-23 11:37:17 -0300

    ensure user is created when correct credentials are provided

---
+++ __tests__/User.spec.ts
@@ -0,0 +1,28 @@
+import request from "supertest";
+import { getConnection } from "typeorm";
+
+import app from "../src/app";
+import createConnection from "../src/database";
+
+describe("Users", () => {
+  beforeAll(async () => {
+    const connection = await createConnection();
+    await connection.runMigrations();
+  });
+
+  afterAll(async () => {
+    const connection = getConnection();
+    await connection.dropDatabase();
+    await connection.close();
+  });
+
+  test("should be able to create a new user", async () => {
+    const response = await request(app).post("/users").send({
+      name: "User Example",
+      email: "user@example.com",
+      password_hash: "123456"
+    });
+
+    expect(response.status).toBe(201);
+  });
+});
