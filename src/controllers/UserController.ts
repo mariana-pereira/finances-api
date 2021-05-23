7710d1edd738a39cbdbcadef5d61d13d8cdb7eea
Mariana <marianadealmeidapereira@outlook.com>
2021-05-23 10:23:23 -0300

    add delete user method to user controller

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 
 import CreateUserService from '../services/createUserService';
+import DeleteUserService from '../services/deleteUserService';
 import ShowUserService from '../services/showUserService';
 
 class UserController {
@@ -35,6 +36,20 @@ class UserController {
       delete user.password_hash;
 
       return response.status(200).json(user);
+    } catch (error) {
+      return response.status(400).json({ error: error.message });
+    }
+  }
+
+  public async delete (request: Request, response: Response): Promise<Response> {
+    try {
+      const { id } = request.params;
+
+      const deleteUser = new DeleteUserService();
+
+      await deleteUser.execute({ id });
+
+      return response.status(200).json({ message: 'User successfully deleted.'});
     } catch (error) {
       return response.status(400).json({ error: error.message });
     }
