32edb070a430eeb8076a3197c2cdcda29f55436c
Mariana <marianadealmeidapereira@outlook.com>
2021-05-30 09:46:09 -0300

    add session routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express';
 
+import sessionsRouter from './session.routes';
 import usersRouter from './user.routes';
 
 const routes = Router();
 
 routes.use('/users', usersRouter);
+
+routes.use('/session', sessionsRouter);
 
 export default routes;
