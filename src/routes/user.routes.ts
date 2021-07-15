4935b2aee298fb0e89793cb2cd5a9ed20b4f28b9
Mariana <marianadealmeidapereira@outlook.com>
2021-07-15 20:39:39 -0300

    refactor: move user controller to modules

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 
-import UserController from '@controllers/UserController';
+import UserController from '@modules/users/usecases/user-controller';
 
 import authMiddleware from '../middlewares/authMiddleware';
 
