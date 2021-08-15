1341d3b144cfe7f2ad312c8820cf2e1e6dcb112f
Mariana <marianna.22sp@gmail.com>
2021-08-15 11:14:05 -0300

    feat: add target controller

---
+++ src/modules/targets/usecases/target-controller.ts
@@ -0,0 +1,26 @@
+import { Request, Response } from 'express';
+
+import { CreateTargetUseCase } from './create-target-usecase';
+
+class TargetController {
+  public async store (request: Request, response: Response): Promise<Response> {
+    try {
+      const { name, deadline, necessary_amount } = request.body;
+
+      const createTarget = new CreateTargetUseCase();
+
+      const account = await createTarget.execute({
+        name,
+        deadline,
+        necessary_amount,
+        user_id: request.user.id
+      });
+
+      return response.status(201).json(account);
+    } catch (error) {
+      return response.status(400).json({ error: error.message });
+    }
+  }
+}
+
+export default new TargetController();
