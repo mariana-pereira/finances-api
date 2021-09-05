9ba643d715c0931a8b0178a347eae5a07b7715a3
Mariana <marianna.22sp@gmail.com>
2021-09-05 10:50:53 -0300

    refactor: move show account to a separate controller

--- src/routes/account.routes.ts
+++ src/routes/account.routes.ts
@@ -3,6 +3,7 @@ import { Router } from 'express';
 import CreateAccountController from '@modules/accounts/usecases/create-account/create-account-controller';
 import DeleteAccountController from '@modules/accounts/usecases/delete-account/delete-account-controller';
 import ListAccountsController from '@modules/accounts/usecases/list-accounts/list-accounts-controller';
+import ShowAccountController from '@modules/accounts/usecases/show-account/show-account-controller';
 
 import authMiddleware from '../middlewares/authMiddleware';
 
@@ -14,7 +15,7 @@ accountsRouter.post('/', CreateAccountController.handle);
 
 accountsRouter.get('/', ListAccountsController.handle);
 
-// accountsRouter.get('/:id', AccountController.show);
+accountsRouter.get('/:id', ShowAccountController.handle);
 
 // accountsRouter.put('/:id', AccountController.update);
 
