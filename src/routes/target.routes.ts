242b67e39504faa206c3680b0d0727cdb2a46ece
Mariana <marianna.22sp@gmail.com>
2021-09-05 11:32:39 -0300

    refactor: move show target to a separate controller

--- src/routes/target.routes.ts
+++ src/routes/target.routes.ts
@@ -3,6 +3,7 @@ import { Router } from 'express';
 import CreateTargetController from '@modules/targets/usecases/create-target/create-target-controller';
 import DeleteTargetController from '@modules/targets/usecases/delete-target/delete-target-controller';
 import ListTargetsController from '@modules/targets/usecases/list-targets/list-targets-controller';
+import ShowTargetController from '@modules/targets/usecases/show-target/show-target-controller';
 
 import authMiddleware from '../middlewares/authMiddleware';
 
@@ -14,7 +15,7 @@ targetsRouter.post('/', CreateTargetController.handle);
 
 targetsRouter.get('/', ListTargetsController.handle);
 
-// targetsRouter.get('/:id', TargetController.show);
+targetsRouter.get('/:id', ShowTargetController.handle);
 
 // targetsRouter.put('/:id', TargetController.update);
 
