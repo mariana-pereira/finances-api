fa0177546f766fd9bb547f8347dcbd29a9de0821
Mariana <marianna.22sp@gmail.com>
2021-09-05 10:55:49 -0300

    refactor: move update account to a separate controller

--- src/routes/account.routes.ts
+++ src/routes/account.routes.ts
@@ -4,6 +4,7 @@ import CreateAccountController from '@modules/accounts/usecases/create-account/c
 import DeleteAccountController from '@modules/accounts/usecases/delete-account/delete-account-controller';
 import ListAccountsController from '@modules/accounts/usecases/list-accounts/list-accounts-controller';
 import ShowAccountController from '@modules/accounts/usecases/show-account/show-account-controller';
+import UpdateAccountController from '@modules/accounts/usecases/update-account/update-account-controller';
 
 import authMiddleware from '../middlewares/authMiddleware';
 
@@ -17,7 +18,7 @@ accountsRouter.get('/', ListAccountsController.handle);
 
 accountsRouter.get('/:id', ShowAccountController.handle);
 
-// accountsRouter.put('/:id', AccountController.update);
+accountsRouter.put('/:id', UpdateAccountController.handle);
 
 accountsRouter.delete('/:id', DeleteAccountController.handle);
 
