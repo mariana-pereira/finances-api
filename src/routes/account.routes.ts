b579647e7f82b63738ca33aef71b1ebb5db95c5b
Mariana <marianna.22sp@gmail.com>
2021-09-05 10:45:33 -0300

    refactor: move list accounts to  a separate controller

--- src/routes/account.routes.ts
+++ src/routes/account.routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 
 import CreateAccountController from '@modules/accounts/usecases/create-account/create-account-controller';
 import DeleteAccountController from '@modules/accounts/usecases/delete-account/delete-account-controller';
+import ListAccountsController from '@modules/accounts/usecases/list-accounts/list-accounts-controller';
 
 import authMiddleware from '../middlewares/authMiddleware';
 
@@ -11,7 +12,7 @@ accountsRouter.use(authMiddleware);
 
 accountsRouter.post('/', CreateAccountController.handle);
 
-// accountsRouter.get('/', AccountController.index);
+accountsRouter.get('/', ListAccountsController.handle);
 
 // accountsRouter.get('/:id', AccountController.show);
 
