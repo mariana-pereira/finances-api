6e0e6e8e55df3a9209df54c628361ed28b2e583a
Mariana <marianna.22sp@gmail.com>
2021-09-06 13:52:58 -0300

    feat: add import transactions route

--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -1,15 +1,20 @@
 import { Router } from 'express';
 
+import multerConfig from '@config/multer';
 import CreateTransactionController from '@modules/transactions/usecases/create-transaction/create-transaction-controller';
+import ImportTransactionsController from '@modules/transactions/usecases/import-transactions/import-transactions-controller';
 import ListAllTransactionsController from '@modules/transactions/usecases/list-all-transactions/list-all-transactions-controller';
 
 import authMiddleware from '../middlewares/authMiddleware';
+
 
 const transactionsRouter = Router();
 
 transactionsRouter.use(authMiddleware);
 
 transactionsRouter.post('/', CreateTransactionController.handle);
+
+transactionsRouter.post('/import', multerConfig.single('file'), ImportTransactionsController.handle);
 
 transactionsRouter.get('/', ListAllTransactionsController.handle);
 
