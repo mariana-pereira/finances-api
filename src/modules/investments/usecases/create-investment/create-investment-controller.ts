019269ff553a952dc5c6c730d8cfc9cc1edf54d0
Mariana <marianna.22sp@gmail.com>
2021-10-01 20:52:11 -0300

    feat: add investment validation

--- src/modules/investments/usecases/create-investment/create-investment-controller.ts
+++ src/modules/investments/usecases/create-investment/create-investment-controller.ts
@@ -1,10 +1,22 @@
 import { Request, Response } from 'express';
+import * as Yup from 'yup';
 
 import { CreateInvestmentUseCase } from './create-investment-usecase';
 
 class CreateInvestmentController {
   public async handle (request: Request, response: Response): Promise<Response> {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      type: Yup.string().required(),
+      tax: Yup.string().required(),
+      application_date: Yup.date().required(),
+      redeem_date: Yup.date().required(),
+      amount: Yup.number().required()
+    });
+
     try {
+      await schema.validate(request.body);
+
       const { name, type, tax, application_date, redeem_date, amount, account_id, target_id } = request.body;
 
       const createInvestment = new CreateInvestmentUseCase();
