b3ce33d007e1d8342eb753d9e671e973f5a4058d
Mariana <marianna.22sp@gmail.com>
2021-10-02 19:49:43 -0300

    feat: add update card route

--- src/routes/card.routes.ts
+++ src/routes/card.routes.ts
@@ -3,6 +3,7 @@ import { Router } from 'express';
 import CreateCardController from '@modules/cards/usecases/create-card/create-card-controller';
 import ListCardsController from '@modules/cards/usecases/list-cards/list-cards-controller';
 import ShowCardController from '@modules/cards/usecases/show-card/show-card-controller';
+import UpdateCardController from '@modules/cards/usecases/update-card/update-card-controller';
 
 import authMiddleware from '../middlewares/authMiddleware';
 
@@ -15,5 +16,7 @@ cardsRouter.post('/', CreateCardController.handle);
 cardsRouter.get('/', ListCardsController.handle);
 
 cardsRouter.get('/:id', ShowCardController.handle);
+
+cardsRouter.put('/:id', UpdateCardController.handle);
 
 export default cardsRouter;
