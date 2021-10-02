961ecb69e7d065f923e463ea66a95c61a02f6dd3
Mariana <marianna.22sp@gmail.com>
2021-10-02 19:26:17 -0300

    feat: add list cards route

--- src/routes/card.routes.ts
+++ src/routes/card.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 
-import CreateCardController from '@modules/cards/create-card/create-card-controller';
+import CreateCardController from '@modules/cards/usecases/create-card/create-card-controller';
+import ListCardsController from '@modules/cards/usecases/list-cards/list-cards-controller';
 
 import authMiddleware from '../middlewares/authMiddleware';
 
@@ -9,5 +10,7 @@ const cardsRouter = Router();
 cardsRouter.use(authMiddleware);
 
 cardsRouter.post('/', CreateCardController.handle);
+
+cardsRouter.get('/', ListCardsController.handle);
 
 export default cardsRouter;
