dcbd8f46f13f7b16de44dbeb62af2bbc5da191ec
Mariana <marianna.22sp@gmail.com>
2021-10-02 19:34:50 -0300

    feat: add show card route

--- src/routes/card.routes.ts
+++ src/routes/card.routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 
 import CreateCardController from '@modules/cards/usecases/create-card/create-card-controller';
 import ListCardsController from '@modules/cards/usecases/list-cards/list-cards-controller';
+import ShowCardController from '@modules/cards/usecases/show-card/show-card-controller';
 
 import authMiddleware from '../middlewares/authMiddleware';
 
@@ -12,5 +13,7 @@ cardsRouter.use(authMiddleware);
 cardsRouter.post('/', CreateCardController.handle);
 
 cardsRouter.get('/', ListCardsController.handle);
+
+cardsRouter.get('/:id', ShowCardController.handle);
 
 export default cardsRouter;
