6dae8f8f983c62b32849c24fcfa016a78321c2d2
Mariana <mariprr22@gmail.com>
2023-03-30 19:09:03 -0300

    fix: move account services to a folder

--- src/application/modules/account.module.ts
+++ src/application/modules/account.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AccountController } from 'src/http/controllers/account/account.controller';
-import { AccountService } from '../services/account.service';
+import { AccountService } from '../services/account/account.service';
 
 @Module({
   controllers: [AccountController],
--- src/http/controllers/account/account.controller.ts
+++ src/http/controllers/account/account.controller.ts
@@ -1,7 +1,7 @@
 import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { GetUser } from 'src/auth/decorators';
-import { AccountService } from 'src/application/services/account.service';
+import { AccountService } from 'src/application/services/account/account.service';
 import { CreateAccountDto } from 'src/http/dtos/account';
 
 @UseGuards(AuthGuard('jwt'))
