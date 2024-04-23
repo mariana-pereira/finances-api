8ee06d6dbb955b166bc8a44fcda9b34827e6686e
Mariana <mariprr22@gmail.com>
2024-04-23 16:17:34 -0300

    fix: add adjustments for vercel

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './modules/user.module';
 import { AuthModule } from './modules/auth.module';
-import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './modules/prisma.module';
 import { AccountModule } from './modules/account.module';
 
