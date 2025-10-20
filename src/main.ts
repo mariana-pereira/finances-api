4d328a2aa3b97bbb416f19d188dca187faa1c652
Mariana Pereira <mariprr22@gmail.com>
2025-10-20 06:33:59 -0300

    fix: cors config

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-  origin: ['https://finances-ijll339pk-marianapereiras-projects.vercel.app',
+  origin: ['https://finances-web-tau.vercel.app',
     'http://localhost:3000'
   ],
   credentials: true,
